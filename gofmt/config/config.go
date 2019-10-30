7ef4ec818eb2a43c066060caba6150ab64313e0c
Nick Miyake <nmiyake@palantir.com>
2019-10-30 16:01:54 -0700

    Use Go modules

--- gofmt/config/config.go
+++ gofmt/config/config.go
@@ -16,7 +16,7 @@ package config
 
 import (
 	"github.com/palantir/godel-format-asset-gofmt/gofmt"
-	"github.com/palantir/godel-format-asset-gofmt/gofmt/config/internal/v0"
+	v0 "github.com/palantir/godel-format-asset-gofmt/gofmt/config/internal/v0"
 )
 
 type Gofmt v0.Config
